32a05ce7bfcc07d8b9dca6e15df515c57598b0e9
Iñaki Elcoro <ielcoro@asemblia.com>
2016-05-08 14:05:09 +0200

    Use ambient module definition

--- i18next-localstorage-cache.d.ts
+++ i18next-localstorage-cache.d.ts
@@ -1,15 +1,18 @@
-declare interface i18nCacheOptions {
-    enabled: boolean;
-    prefix: string;
-    expirationTime: number;
-}
-
-export declare interface Cache {
-    constructor(services? : any, options? : i18nCacheOptions);
-}
-
 declare namespace I18Next {
+    interface i18nCacheOptions {
+        enabled: boolean;
+        prefix: string;
+        expirationTime: number;
+    }
+
     export interface Options {
         cache: i18nCacheOptions;
+    }
+
+}
+
+declare module 'i18next-localstorage-cache' {
+    export interface Cache {
+        constructor(services?: any, options?: I18Next.i18nCacheOptions);
     }
 }
--- i18next-xhr-backend.d.ts
+++ i18next-xhr-backend.d.ts
@@ -1,17 +1,19 @@
-export declare interface i18nXHROptions {
-    loadPath: string;
-    addPath: string;
-    allowMultiLoading : boolean;
-    crossdomain: boolean;
-    ajax : (url : string, options : any, callback : any, data : any);
-}
-
-export declare interface XHR {
-    constructor(services? : any, options? : i18nXHROptions);
-}
-
 declare namespace I18Next {
-    export interface Options {
+    interface i18nXHROptions {
+        loadPath: string;
+        addPath: string;
+        allowMultiLoading: boolean;
+        crossdomain: boolean;
+        ajax: (url: string, options: any, callback: any, data: any) => {};
+    }
+
+    interface Options {
         backend: i18nXHROptions;
+    }
+}
+
+declare module 'i18next-xhr-backend' {
+    export interface XHR {
+        constructor(services?: any, options?: I18Next.i18nXHROptions);
     }
 }
