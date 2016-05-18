4d6d6608b3a04542d149682a6d91b4930518df15
Iñaki Elcoro <ielcoro@asemblia.com>
2016-05-18 20:21:25 +0200

    Twbs-pagination definition

---
+++ twbs-pagination.d.ts
@@ -0,0 +1,19 @@
+declare namespace TwbsPagination {
+    interface Options {
+        totalPages : number,
+        startPage? : number,
+        visiblePages? : number,
+        initiateStartPageClick? : boolean,
+        href? : boolean,
+        first? : string,
+        prev? : string,
+        last? : string,
+        loop? : boolean,
+        paginationClass? : string,
+        onPageClick : (event : Event, page : number) => void
+    }
+}
+
+interface JQuery {
+   twbsPagination : (opts : TwbsPagination.Options) => void; 
+}
